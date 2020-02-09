c14a77725ddfe158c943aeb86d457bbf6d8c0ed3
Julio <juliobell2014@gmail.com>
2020-02-08 20:11:49 -0500

    created addItem method into budgetController

--- app.js
+++ app.js
@@ -36,17 +36,75 @@
 //BUDGET CONTROLLER
 var budgetController = (function() {
 
-    //private variable and function in the Closure
-    
+    //private variables and functions in the Closure
+
+    //Function Constructor for Expense 
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    //Function Constructor for Income
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    //data structure for storing all expenses, incomes and totals
+    var data = {
+        allItems : {
+            exp: [],
+            inc: []
+        },
+        totals : {
+            exp: 0,
+            inc: 0
+        }
+    };
 
     //Object is returned in the budgetController variable
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            //last item in the array
+            var last = data.allItems[type].length - 1;
+
+            //if array is not empty create new ID
+            if (data.allItems[type].length > 0) {
+                //Create new ID
+                ID = data.allItems[type][last].id + 1;
+            //if array is empty set ID to zero    
+            } else {
+                ID = 0;
+            }
+            
+
+            //Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+            //add new item into the corresponding array
+            data.allItems[type].push(newItem);
+
+            //return the new element
+            return newItem;
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+    };
         
 })();
 
 //UI CONTROLLER
 var UIController = (function() {
     var DOMstrings = {
-        inputType : '.add__type',
+        inputType : '.add__type', //will be either 'inc' or 'exp'
         inputDescription : '.add__description',
         inputValue : '.add__value',
         inputAddBtn : '.add__btn'
@@ -90,10 +148,13 @@ var appController = (function(budgetCtrl, UICtrl) {
     }
 
     var ctrlAddItem = function() {
+        //variable declaration
+        var input, newItem;
         //1. Get the field input data
-        var input = UICtrl.getInput();
+        input = UICtrl.getInput();
             // console.log(input);
         //2. Add the item to the budget controller
+        newItem = budgetController.addItem(input.type, input.description, input.value);
         //3. Add the new item to the UI
         //4. Calculate the budget
         //5. Display the budget on the UI
